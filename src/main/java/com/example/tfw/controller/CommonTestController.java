7b8f09dc23d6dbe4c31ed8a57cc3bd996f2f07c5
ToZero <hpu.player@gmail.com>
2023-01-07 19:18:13 +0800

    测试ieda提交git

--- src/main/java/com/example/tfw/controller/CommonTestController.java
+++ src/main/java/com/example/tfw/controller/CommonTestController.java
@@ -1,6 +1,7 @@
 package com.example.tfw.controller;
 
 import com.example.tfw.mapper.UserMapper;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,13 @@ public class CommonTestController {
     private UserMapper userMapper;
 
     @GetMapping("/hello")
+    @ApiOperation("普通测试")
     public String commonTest(){
         return userMapper.selectUserByUUID("123").toString();
+    }
+    
+    @GetMapping("/commitGit")
+    public String githubTest(){
+        return "success";
     }
 }
